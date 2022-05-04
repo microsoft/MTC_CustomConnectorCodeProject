0c0d8a869817da89b0c8caad0311dd4ceb67b7c4
richross <rich.ross@live.com>
2022-05-04 07:42:38 -0400

    added some comments and description to the program flow.

--- codeSegment/Program.cs
+++ codeSegment/Program.cs
@@ -1,20 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Net.Http.Headers;
 
-Console.WriteLine("Hello, World!");
-
 try
 {
     //create the HttpRequest that matches the request on the custom connector
-    HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, "http://conferenceapi.azurewebsites.net/sessions");
+    HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, "[REPLACE WITH CUSTOM CONNECTOR URL]");
     requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-    requestMessage.Headers.Add("Ocp-Apim-Subscription-Key", "5003d038b3b84b499d8fca900b0b61e2");
+    //ADD ANY ADDITIONAL HEADERS HERE
 
+    //API-Management Subscription Key
+    // requestMessage.Headers.Add("Ocp-Apim-Subscription-Key", "[REPLACE WITH YOUR SUBSCRIPTION KEY]");
+]
+    //create a new script object
     var myScript = new Script();
+
+    //create a scriptcontext object and assign it to the Context property of the script class
     var myScriptContext = new ScriptContext("GetSessions", requestMessage);
     myScript.Context = myScriptContext;    
 
+    //use the script object to execute the api call
     HttpResponseMessage response = myScript.ExecuteAsync().ConfigureAwait(true).GetAwaiter().GetResult();
+    
+    //write the results to the console.
     Console.WriteLine(response.Content.ReadAsStringAsync().Result);
 }
 catch (Exception ex)
